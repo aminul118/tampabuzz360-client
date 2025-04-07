56ccac7b121e63398fd21dd12ae00b060d568eda
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-08 01:49:45 +0600

    contact page resolve

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,17 +1,11 @@
-import ContactForm from "@/components/Contact/ContactForm";
-import ContactInfo from "@/components/Contact/ContactInfo";
 
-const Page = () => {
+
+const page = () => {
   return (
-    <section className="min-h-screen bg-gradient-to-r from-blue-600 via-blue-800 to-blue-900 dark:from-gray-700 dark:via-gray-800 dark:to-gray-900">
-      <div className="container flex flex-col min-h-screen px-6 py-12 mx-auto">
-        <div className="flex-1 lg:flex lg:items-center lg:-mx-6">
-          <ContactInfo />
-          <ContactForm />
-        </div>
-      </div>
-    </section>
+    <div>
+      
+    </div>
   );
 };
 
-export default Page;
+export default page;

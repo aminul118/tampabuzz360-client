a40b48691bb991914202dd7dbfbfdb5c7997fa12
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-08 10:52:49 +0600

    Dark mood tittle change

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -8,7 +8,9 @@ type TitleType = {
 const Title = ({ title, description }: TitleType) => {
   return (
     <div className="pb-6 lg:pb-8 max-w-2xl mx-auto text-center">
-      <h1 className="text-3xl lg:text-4xl font-bold text-[#1F5781]">{title}</h1>
+      <h1 className="text-3xl lg:text-4xl font-bold text-[#1F5781] dark:text-white">
+        {title}
+      </h1>
       {description && <p className="mt-2">{description}</p>}
     </div>
   );

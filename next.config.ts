8d0b6688fa47119ab1677f1d1303cfd5bbf2c3b5
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-09 11:59:47 +0600

    Strict-Transport-Security update

--- next.config.ts
+++ next.config.ts
@@ -30,19 +30,18 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value: `
-    default-src 'self';
-    script-src 'self';
-    style-src 'self' https://fonts.googleapis.com;
-    font-src 'self' https://fonts.gstatic.com;
-    img-src 'self' https://i.ibb.co https://i.ibb.co.com;
-    connect-src 'self';
-    object-src 'none';
-    frame-ancestors 'none';
-  `
+              default-src 'self';
+              script-src 'self';
+              style-src 'self' https://fonts.googleapis.com;
+              font-src 'self' https://fonts.gstatic.com;
+              img-src 'self' https://i.ibb.co;
+              connect-src 'self';
+              object-src 'none';
+              frame-ancestors 'none';
+            `
               .replace(/\s{2,}/g, " ")
               .trim(),
           },
-
           {
             key: "X-Frame-Options",
             value: "DENY",

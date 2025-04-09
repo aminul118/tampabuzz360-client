176a1a73da0cb020a4738c7178dd0de0635c6c94
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-09 11:51:24 +0600

    Content-Security-Policy update

--- next.config.ts
+++ next.config.ts
@@ -29,9 +29,20 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://i.ibb.co https://i.ibb.co.com; connect-src 'self'; object-src 'none'; frame-ancestors 'none';",
+            value: `
+    default-src 'self';
+    script-src 'self';
+    style-src 'self' https://fonts.googleapis.com;
+    font-src 'self' https://fonts.gstatic.com;
+    img-src 'self' https://i.ibb.co https://i.ibb.co.com;
+    connect-src 'self';
+    object-src 'none';
+    frame-ancestors 'none';
+  `
+              .replace(/\s{2,}/g, " ")
+              .trim(),
           },
+
           {
             key: "X-Frame-Options",
             value: "DENY",

1dbc034fe4ec57d0153de5598b7732e67d153637
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-09 11:35:21 +0600

    custom security added in next config

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,41 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  // Adding custom headers for security
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https://i.ibb.co https://i.ibb.co.com; connect-src 'self'; font-src 'self'; object-src 'none'; frame-ancestors 'none';",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(self), microphone=()',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
